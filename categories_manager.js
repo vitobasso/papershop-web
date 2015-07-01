29c3de7fdb0161e0e536f6f29a37e52d6129acab
Victor Basso <vitobasso@gmail.com>
2015-07-01 19:10:06 -0300

    histograms refactored

---
+++ categories_manager.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Victor on 01/07/2015.
+ */
+function Categories(ebay){
+
+    var categories = [];
+
+    this.addFromItens = function (itens) {
+        categories = uniqueCategories(itens);
+        populate();
+    };
+
+    function uniqueCategories(items) {
+        var mapByCategoryId = d3.map(items, function (item) {
+            return item.category.id
+        });
+        var categoryIds = mapByCategoryId.keys();
+        return categoryIds.map(function (id) {
+            var item = mapByCategoryId.get(id);
+            return item.category;
+        });
+    }
+
+    function populate() {
+        d3.select("#categories")
+            .selectAll("li").data(categories)
+            .enter()
+            .append("li")
+            .html(function (category) {
+                return category.name;
+            })
+            .attr("title", function (category) {
+                return category.id;
+            })
+            .on("click", populateAspects)
+            .append("ul")
+    }
+
+    function populateAspects(category) {
+        ebay.histograms({categoryId: category.id}, function (response) {
+            var selCategory = d3.select("#categories")
+                .select("[title='" + category.id + "']");
+
+            var aspects = parseHistogramsResponse(response);
+            var selAspect = selCategory.select("ul")
+                .selectAll("li").data(aspects)
+                .enter().append("li")
+                .html(function (aspect) {
+                    return aspect.name;
+                });
+
+            var selPartition = selAspect.append("ul")
+                .selectAll("li").data(function (aspect) {
+                    return aspect.partitions;
+                })
+                .enter().append("li")
+                .html(function (partition) {
+                    return partition.name;
+                });
+        });
+    }
+
+    function parseHistogramsResponse(response) {
+        var aspects = response.getHistogramsResponse[0].aspectHistogramContainer[0].aspect;
+        return aspects.map(function (aspect) {
+            return {
+                name: aspect['@name'],
+                partitions: aspect.valueHistogram.map(function (histogram) {
+                    return {
+                        name: histogram['@valueName'],
+                        count: histogram.count[0]
+                    }
+                })
+            }
+        });
+    }
+}
--- ebay_chart.js
+++ ebay_chart.js
@@ -19,11 +19,12 @@ function EbayChart() {
     };
     var chart = new Chart(x, y);
 
-    var categories = [];
     var items = [];
     var currentPage = 0;
 
     var ebay = new Ebay();
+
+    var categories = new Categories(ebay);
 
     this.requestNextPage = function () {
         currentPage = currentPage + 1;
@@ -38,8 +39,7 @@ function EbayChart() {
     function populateChart(response) {
         var newItems = parseFindResponse(response);
         items = items.concat(newItems);
-        categories = uniqueCategories(items);
-        populateCategories(categories);
+        categories.addFromItens(items);
         chart.populate(items);
     }
 
@@ -63,70 +63,5 @@ function EbayChart() {
     }
 
     var dateFormat = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ");
-
-    function populateCategories(categories) {
-        d3.select("#categories")
-            .selectAll("li").data(categories)
-            .enter()
-            .append("li")
-            .html(function (category) {
-                return category.name;
-            })
-            .attr("title", function (category) {
-                return category.id;
-            })
-            .on("click", populateAspects)
-            .append("ul")
-    }
-
-    function uniqueCategories(items) {
-        var mapByCategoryId = d3.map(items, function (item) {
-            return item.category.id
-        });
-        var categoryIds = mapByCategoryId.keys();
-        return categoryIds.map(function (id) {
-            var item = mapByCategoryId.get(id);
-            return item.category;
-        });
-    }
-
-    function populateAspects(category) {
-        ebay.histograms({categoryId: category.id}, function (response) {
-            var selCategory = d3.select("#categories")
-                .select("[title='" + category.id + "']");
-
-            var aspects = parseHistogramsResponse(response);
-            var selAspect = selCategory.select("ul")
-                .selectAll("li").data(aspects)
-                .enter().append("li")
-                .html(function (aspect) {
-                    return aspect.name;
-                });
-
-            var selPartition = selAspect.append("ul")
-                .selectAll("li").data(function (aspect) {
-                    return aspect.partitions;
-                })
-                .enter().append("li")
-                .html(function (partition) {
-                    return partition.name;
-                });
-        });
-    }
-
-    function parseHistogramsResponse(response) {
-        var aspects = response.getHistogramsResponse[0].aspectHistogramContainer[0].aspect;
-        return aspects.map(function (aspect) {
-            return {
-                name: aspect['@name'],
-                partitions: aspect.valueHistogram.map(function (histogram) {
-                    return {
-                        name: histogram['@valueName'],
-                        count: histogram.count[0]
-                    }
-                })
-            }
-        });
-    }
 
 }
