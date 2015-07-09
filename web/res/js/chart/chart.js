be3fd984b63149d50437b1711c5b49bc439f6486
Victor Basso <vitobasso@gmail.com>
2015-07-09 20:32:53 -0300

    refactored chart

--- web/res/js/chart/chart.js
+++ web/res/js/chart/chart.js
@@ -6,6 +6,7 @@ function EbayChart(api, getItems, axisSelectorId, colorSelectorId) {
 
     var categories = new Categories(api);
     var axes = new ChartAxes(categories);
+    var tooltip = new ChartTooltip();
     var chart;
 
     this.populate = function (items) {
@@ -25,7 +26,7 @@ function EbayChart(api, getItems, axisSelectorId, colorSelectorId) {
 
         var xAxis = getSelected(axisSelectorId);
         var colorAxis = getSelected(colorSelectorId);
-        chart = new Chart(axes.priceAxis, xAxis, colorAxis, renderTooltip);
+        chart = new Chart(axes.priceAxis, xAxis, colorAxis, tooltip.render);
 
         var items = getItems();
         if (items) {
@@ -55,36 +56,6 @@ function EbayChart(api, getItems, axisSelectorId, colorSelectorId) {
             });
     }
 
-    function renderTooltip() {
-        var item = this.__data__;
-        var priceStr = item.price.currency + " " + item.price.value;
-        return "<div class='chart-tooltip'>" +
-            "<img src='" + item.image + "'/>" +
-            "<p>" + item.title + "</p>" +
-            "<p>" + "Price: " + priceStr + "</p>" +
-            "<p>" + "Category: " + item.category.name + "</p>" +
-            "<p>" + "Condition: " + item.condition.name + "</p>" +
-            renderAspectsForTooltip(item) +
-            "</div>";
-    }
-
-    function renderAspectsForTooltip(item) {
-        var result = "";
-        for (var aspectName in item.aspects) {
-            if (item.aspects.hasOwnProperty(aspectName)) {
-                result += "<p>" + aspectName + ": " + renderAspectValueForTooltip(item.aspects[aspectName]) + "</p>";
-            }
-        }
-        return result;
-    }
-
-    function renderAspectValueForTooltip(aspectValue) {
-        var result = aspectValue.value;
-        if (aspectValue.confidence < 1) {
-            result += " (?" + Number(aspectValue.confidence).toFixed(2) + ")";
-        }
-        return result;
-    }
 
     populateSelectors();
     rebuildChart();
