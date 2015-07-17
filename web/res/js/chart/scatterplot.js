0e2101f2be055d023da6f48a552b8f77fe9dabae
Victor Basso <vitobasso@gmail.com>
2015-07-17 17:28:22 -0300

    chart: force layout, collisions

--- web/res/js/chart/scatterplot.js
+++ web/res/js/chart/scatterplot.js
@@ -9,7 +9,7 @@ function ScatterPlot(parentDivId) {
 
     var svg, canvas;
     var yParam, xParam, colorParam;
-    var x, y, colorScale;
+    var xScale, yScale, colorScale;
     var axes;
     var _data;
 
@@ -26,7 +26,7 @@ function ScatterPlot(parentDivId) {
         render()
     };
 
-    this.colorScale = function(datum) {
+    this.colorScale = function (datum) {
         return colorScale(datum);
     };
 
@@ -75,20 +75,20 @@ function ScatterPlot(parentDivId) {
     }
 
     function createScales() {
-        x = setXRange(xParam.getScale());
-        y = setYRange(yParam.getScale());
+        xScale = setXRange(xParam.getScale());
+        yScale = setYRange(yParam.getScale());
         colorScale = setColorRange(colorParam.getScale());
     }
 
     function populate() {
         updateDomains();
         axes.update();
-        renderCircles();
+        startLayout();
     }
 
     function updateDomains() {
-        xParam.updateDomain(x, _data);
-        yParam.updateDomain(y, _data);
+        xParam.updateDomain(xScale, _data);
+        yParam.updateDomain(yScale, _data);
         colorParam.updateDomain(colorScale, _data);
     }
 
@@ -97,7 +97,7 @@ function ScatterPlot(parentDivId) {
     function renderCircles() {
         var circles = canvas.selectAll("circle").data(_data, getId);
 
-        circles.transition()
+        circles
             .call(positionCircle);
 
         circles.enter()
@@ -111,34 +111,139 @@ function ScatterPlot(parentDivId) {
 
         circles.exit()
             .remove();
-
     }
 
     function positionCircle(sel) {
-        sel.attr("cx", function (datum) {
-            return x(xParam.getProperty(datum))
-        })
-            .attr("cy", function (datum) {
-                return y(yParam.getProperty(datum))
-            })
-            .style("fill", function (datum) {
-                return colorScale(colorParam.getProperty(datum))
-            });
+        sel.attr("cx", getX)
+            .attr("cy", getY)
+            .style("fill", getColor);
+    }
+
+    function getX(datum) {
+        return datum.point.x;
+    }
+
+    function getY(datum) {
+        return datum.point.y;
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////
 
+    function startLayout() {
+        initPositions(_data);
+        var points = _data.map(getPoint);
+
+        var force = d3.layout.force()
+            .size([width, height])
+            .gravity(0)
+            .charge(0)
+            .nodes(points);
+
+        force.on("tick", function (e) {
+            moveTorwardsTarget(e);
+            avoidCollisions(points);
+            renderCircles();
+        });
+
+        force.start();
+    }
+
+    function moveTorwardsTarget(e) {
+        var cooling = 0.1 * e.alpha;
+        _data.forEach(function (datum, i) {
+            var target = getTargetPosition(datum);
+            var point = datum.point;
+            point.y += (target.y - point.y) * cooling;
+            point.x += (target.x - point.x) * cooling;
+        });
+    }
+
+    function avoidCollisions(points) {
+        var q = d3.geom.quadtree(points);
+        points.forEach(function (point) {
+            var adjustPosition = collide(point);
+            q.visit(adjustPosition);
+        });
+
+    }
+
+    function collide(node) {
+        var r = node.radius,
+            nx1 = node.x - r,
+            nx2 = node.x + r,
+            ny1 = node.y - r,
+            ny2 = node.y + r;
+        return function (quad, x1, y1, x2, y2) {
+            if (quad.point && (quad.point !== node)) {
+                var dx = node.x - quad.point.x,
+                    dy = node.y - quad.point.y,
+                    l = Math.sqrt(dx * dx + dy * dy),
+                    r = node.radius + quad.point.radius;
+                if (l < r) {
+                    if (l > 0) {
+                        l = (l - r) / l * .5;
+                        node.x -= dx *= l;
+                        node.y -= dy *= l;
+                    } else {
+                        node.x += node.radius; //avoid division by 0 when node & quad.point coincide
+                    }
+                    quad.point.x += dx;
+                    quad.point.y += dy;
+                }
+            }
+            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+        };
+    }
+
+    function getPoint(datum) {
+        return datum.point;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+    function initPositions(data) {
+        data.forEach(initPosition);
+    }
+
+    function initPosition(datum) {
+        if (!datum.point) {
+            datum.point = getTargetPosition(datum);
+        }
+    }
+
+    function getTargetPosition(datum) {
+        return {
+            x: getTargetX(datum),
+            y: getTargetY(datum),
+            radius: 3.5
+        }
+    }
+
+    function getTargetX(datum) {
+        return xScale(xParam.getProperty(datum))
+    }
+
+    function getTargetY(datum) {
+        return yScale(yParam.getProperty(datum))
+    }
+
+    function getColor(datum) {
+        return colorScale(colorParam.getProperty(datum))
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
     function Axes() {
 
         var xAxis = d3.svg.axis()
-            .scale(x)
+            .scale(xScale)
             .orient("bottom");
         if (xParam.formatTick) {
             xAxis.tickFormat(xParam.formatTick)
         }
 
         var yAxis = d3.svg.axis()
-            .scale(y)
+            .scale(yScale)
             .orient("left");
 
         this.init = function () {
@@ -172,7 +277,7 @@ function ScatterPlot(parentDivId) {
 
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
 
     function setXRange(scale) {
         if (isOrdinal(scale)) {
