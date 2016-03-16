71907434d3af453290c61326b062613722583e0a
Victor Basso <vitobasso@gmail.com>
2016-03-16 22:40:20 +0100

    api refactor

--- web/res/js/external_api/ebay/api.js
+++ web/res/js/external_api/ebay/api.js
@@ -1,47 +1,18 @@
 
 function EbayApi() {
-    //FIXME duplicates MLApi
 
     var urlBuilder = new EbayUrlBuilder();
     var parser = new EbayResponseParser();
 
     this.find = function (params, onSuccess, onFail) {
         var url = urlBuilder.buildFindUrl(params);
-        requestAndParse(url, parser.parseFind, onSuccess, onFail);
+        ApiCommon.requestAndParse(url, parser.parseFind, onSuccess, onFail);
     };
 
     this.findAspects = function (params, onSuccess, onFail) {
         var url = urlBuilder.buildHistogramsUrl(params);
-        requestAndParse(url, parser.parseHistograms, onSuccess, onFail);
+        ApiCommon.requestAndParse(url, parser.parseHistograms, onSuccess, onFail);
     };
-
-    function requestAndParse(url, parse, onSuccess, onFail) {
-        request(url, parseAndCallback, onFail);
-
-        function parseAndCallback(response) {
-            try {
-                var result = parse(response);
-            } catch (err) {
-                if(onFail){
-                    onFail(err);
-                } else {
-                    throw err;
-                }
-                return;
-            }
-            onSuccess(result);
-        }
-    }
-
-    function request(url, onSuccess, onFail) {
-        console.log(url);
-        $.ajax({
-            url: url,
-            dataType: "jsonp",
-            success: onSuccess,
-            error: onFail
-        })
-    }
 
 }
 
--- web/res/js/external_api/mercadolibre/api.js
+++ web/res/js/external_api/mercadolibre/api.js
@@ -1,42 +1,17 @@
 
 function MLApi() {
-    //FIXME duplicates EbayApi
 
     var urlBuilder = new MLUrlBuilder();
     var parser = new MLResponseParser();
 
     this.find = function (params, onSuccess, onFail) {
         var url = urlBuilder.buildFindUrl(params);
-        requestAndParse(url, parser.parseFind, onSuccess, onFail);
+        ApiCommon.requestAndParse(url, parser.parseFind, onSuccess, onFail);
     };
 
     this.findAspects = function (params, onSuccess, onFail) {
         //TODO remove
     };
-
-    function requestAndParse(url, parse, onSuccess, onFail) {
-        request(url, parseAndCallback, onFail);
-
-        function parseAndCallback(response) {
-            try {
-                var result = parse(response);
-            } catch (err) {
-                onFail(err);
-                return;
-            }
-            onSuccess(result);
-        }
-    }
-
-    function request(url, onSuccess, onFail) {
-        console.log(url);
-        $.ajax({
-            url: url,
-            dataType: "jsonp",
-            success: onSuccess,
-            error: onFail
-        })
-    }
 
 }
 
