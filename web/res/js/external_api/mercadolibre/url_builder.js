83081a3bd31e86b6b4328463eac147e2112a24be
Victor Basso <vitobasso@gmail.com>
2016-03-19 18:22:04 +0100

    bug fix on ml/url_builder

--- web/res/js/external_api/mercadolibre/url_builder.js
+++ web/res/js/external_api/mercadolibre/url_builder.js
@@ -28,12 +28,12 @@ function MLUrlBuilder() {
 
     function buildFilters(params) {
         var result = "";
-        var filterParams = params.filters.filter(isCommonFilter);;
+        var filterParams = params.filters.filter(isCommonFilter);
         if (filterParams) {
             filterParams.forEach(function (filterParam, i) {
                 var values = filterParam.selected.map(getId);
                 //TODO ml allows 1 category, ebay allows multiple. make it clear in the UI
-                result += "&" + filterParam.filter.name + "=" + values[0];
+                result += "&" + filterParam.filter.id + "=" + values[0];
             });
         }
         return result;
