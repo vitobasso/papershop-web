30088d2f94a9ce8c3074dec5bb29a15c0aec8061
Victor Basso <vitobasso@gmail.com>
2016-05-16 11:08:38 +0200

    separated ebay specific config

--- web/res/js/config/config.js
+++ web/res/js/config/config.js
@@ -2,14 +2,8 @@
 var Config = (function () {
     var module = {};
 
-    var selectedSubdomain = SITE_IDS[0];
-    var selectedCountry;
-
     module.init = function () {
-        // setUserLocale();
         populate('#site', Sites.list());
-        // populate('#sub-domain', SITE_IDS);
-        // populate('#country', COUNTRY_CODES);
         HandlerAssigner.bindDialog("#config", "#config-btn");
     };
 
@@ -17,7 +11,7 @@ var Config = (function () {
         var select = d3.select("#config")
             .select(selectId);
 
-        var options = select.selectAll('option').data(data, getId)
+        select.selectAll('option').data(data, getId)
             .enter().append('option')
             .attr("value", getId)
             .html(getName);
@@ -36,32 +30,6 @@ var Config = (function () {
             option = options.filter((d, i) => i === index);
         return option.datum();
     }
-
-    function setUserLocale() {
-        var userCountryCode = getUserCountry();
-        var matchUserCountry = hasId(userCountryCode);
-        var matchDefaultCountry = hasId("US");
-        selectedCountry = COUNTRY_CODES.find(matchUserCountry)
-        || COUNTRY_CODES.find(matchDefaultCountry);
-    }
-
-    function getUserCountry() {
-        var lang = navigator.language;
-        var pattern = /(\w\w)-(\w\w)/i;
-        var regex = new RegExp(pattern);
-        var result = regex.exec(lang);
-        return result[2] || "US";
-    }
-
-    var sites = {
-        name: "Site",
-        values: SITE_IDS
-    };
-
-    var countries = {
-        name: "AvailableTo",
-        values: COUNTRY_CODES
-    };
 
     return module;
 }());
