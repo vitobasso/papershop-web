b71ff26ab1a466582e54f24a31fc4cbfda9d1476
Victor Basso <vitobasso@gmail.com>
2019-10-09 20:32:47 +0100

    auto select axes

--- web/res/js/chart/chart_manager.js
+++ web/res/js/chart/chart_manager.js
@@ -18,6 +18,7 @@ var ChartManager = (function () {
 
     function onNewItems(_, _, filtered) {
         updateAxisOptions();
+        autoSelectAxes();
         setData(filtered);
     }
 
@@ -34,6 +35,25 @@ var ChartManager = (function () {
         axisOptions = AxisFactory.listOptions();
     }
 
+    function autoSelectAxes(){
+        var anyAxisChanged = false
+        if(yAxis.label == "?") {
+            const priceAxis = axisOptions.find(x => x.label.toLowerCase() == "price")
+            const candidate = priceAxis || axisOptions.find(x => x.label != "?")
+            yAxis = candidate || yAxis
+            anyAxisChanged = candidate
+        }
+        if(xAxis.label == "?") {
+            const candidate = axisOptions.filter(x => x.label.toLowerCase() != "price")
+                                         .find(x => x.label != "?")
+            xAxis = candidate || xAxis
+            anyAxisChanged = anyAxisChanged || candidate
+        }
+        if(anyAxisChanged) {
+            buildChart()
+        }
+    }
+
     function buildChart() {
         if (items) ChartRenderer.update(items, yAxis, xAxis);
     }
@@ -42,7 +62,7 @@ var ChartManager = (function () {
 
 
     module.changeXAxisByName = name => {
-        var axis = axisOptions.find(labelEquals(name));
+        var axis = axisOptions.find(x => x.label == name);
         if (axis) {
             xAxis = axis;
             buildChart();
@@ -50,7 +70,7 @@ var ChartManager = (function () {
     };
 
     module.changeYAxisByName = name => {
-        var axis = axisOptions.find(labelEquals(name));
+        var axis = axisOptions.find(y => y.label == name);
         if (axis) {
             yAxis = axis;
             buildChart();
@@ -59,12 +79,6 @@ var ChartManager = (function () {
 
     module.getXAxis = _ => xAxis;
     module.getYAxis = _ => yAxis;
-
-    function labelEquals(name) {
-        return function (axis) {
-            return axis.label == name
-        }
-    }
 
     return module;
 }());
